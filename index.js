da5d428e0823e3b62ce202550df6649980e5c55a
sasa <udongunma@gmail.com>
2023-11-30 16:17:40 +0900

    テスト

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const app = express()
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
   res.send('/docs')
+  res.send('動作1')
 })
 
 app.use(function(req, res, next) {
-  res.status(404).send('/docs/404page.html');
+  res.status(404).send('/docs/404page.html').send('動作2');
+
 });
