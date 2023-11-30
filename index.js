90aac6cb83a7d8b7b5213661fbfb3b8d0d7636aa
sasa <udongunma@gmail.com>
2023-11-30 16:52:08 +0900

    ルートディレクトリ変更への対応

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
-  res.send('/docs')
-  res.send('動作1')
+  res.send('/docs').send('動作1');
 })
 
 app.use(function(req, res, next) {
-  res.status(404).send('/docs/404page.html').send('動作2');
+  res.status(404).send('/docs/404.html').send('動作2');
 
 });
